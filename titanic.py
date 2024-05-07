51fc2ac6f1f7e4bb497d8e6fa460c2f0f35d1f78
Aniket066 <100535216+Aniket066@users.noreply.github.com>
2024-05-07 07:05:08 +0530

    Add files via upload

---
+++ titanic.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split #Training Testing and Spliting the model
+from sklearn.linear_model import LogisticRegression #Using LogisticRegression
+
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
+
+
